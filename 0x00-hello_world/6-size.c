2b217d419b62da1023ed2368e8917189f40caa05
Nada668 <nadaaleraqy@gmail.com>
2023-08-10 11:16:02 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+ * main - A program that prints the size
+ * Return 0 (Success)
+ */
+int main (void)
+{
+         char a;
+         int b;
+         long int C;
+         float f;
+printf("Size of char:%lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of int:%lu byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of a long int:%lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long long int:%lu byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of a float:%lu bytes(s)\n",(unsigned lonsizeof(f));
+return (0);
+}
